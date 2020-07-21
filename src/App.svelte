348e932bb96641bfe140de70040121a561dd1672
Omarosh <omarkhattab59@yahoo.com>
2020-07-21 03:10:43 +0200

    html

--- src/App.svelte
+++ src/App.svelte
@@ -1,2 +1,20 @@
-<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+<script>
+  import HT from "./HT.svelte";
+  import Timer from "./timer.svelte";
+</script>
+
+<style>
+  h1 {
+    text-align: center;
+  }
+</style>
+
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
+
+</head>
 <h1>Test</h1>
+
+<Timer />
+<HT />
+<h3>picture source</h3>
---
+++ src/timer.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Progressbar from "./progressbar.svelte";
+</script>
+
+<style>
+  h2 {
+    margin: 0;
+  }
+  .start {
+    background-color: rgb(154, 73, 12);
+  }
+</style>
+
+<div bp="grid">
+  <h2 bp="offset-5@md 4@md 12@sm">Seconds Left:</h2>
+
+</div>
+
+<Progressbar />
+
+<button class="start">Start</button>
